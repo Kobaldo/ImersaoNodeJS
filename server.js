199be14e598e81e621cbb6004f853767b44a2043
Rodrigo Martins <rodrigo.ifsemg@gmail.com>
2024-11-20 11:23:55 -0300

    Aula3

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 import express from "express";
+import routes from "./src/routes/postRoutes.js";
 
 const app = express();
+routes(app);
+
 app.listen(3000,()=>{
     console.log("Servidor escutando...");
 });
 
+/*
+function buscarPostPorID(id){
+    return posts.findIndex((post)=>{
+        return post.id === Number(id)
+    })
+}
 
-app.get("/api",(req,res)=>{
-    res.status(200).send("Boas vindas à imersão!");
+app.get("/posts/:id",(req,res)=>{
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
 });
+*/
